697f034df4f915590d2fe57abec8f1a78821e368
Heron Rossi <heron.rossi@hotmail.com>
2018-01-21 12:05:33 -0200

    Setting new tshirt app and creating its model

--- tshirt/models.py
+++ tshirt/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Tshirt(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True)
+    brand = models.CharField(max_length=150, null=False)
+    size = models.CharField(max_length=3, null=False)
+    quantity = models.PositiveSmallIntegerField(null=False)
