076eb2897df9c41bdbae7e9bded4794586220d19
Heron Rossi <heron.rossi@hotmail.com>
2018-01-24 12:11:28 -0200

    Refactoring test_views

--- tshirt/tests/test_views.py
+++ tshirt/tests/test_views.py
@@ -9,61 +9,43 @@ from tshirt.views import tshirt_list
 
 @pytest.mark.django_db(transaction=True)
 class TestTshirtCollection:
+    client = Client()
+
     def test_get_a_list_of_tshirts_as_json(self):
-        Tshirt.objects.create(brand='Volcom', quantity=10, size='L', pk=1)
-        Tshirt.objects.create(brand='Zoo York', quantity=5, size='S', pk=2)
-        client = Client()
-        resp = client.get('/tshirt/')
-        assert resp.json() == [{
-            'brand': 'Volcom',
-            'pk': 1,
-            'quantity': 10,
-            'size': 'L'
-        }, {
-            'brand': 'Zoo York',
-            'pk': 2,
-            'quantity': 5,
-            'size': 'S'
-        }]
+        Tshirt.objects.create(brand='Volcom', quantity=10, size='L')
+        Tshirt.objects.create(brand='Zoo York', quantity=5, size='S')
+        resp = self.client.get('/tshirt/')
+        assert len(resp.json()) == len(Tshirt.objects.all())
         assert resp.status_code == 200
 
     def test_post_a_new_tshirt(self):
         post_data = {"brand": "Volcom", "quantity": 10, "size": "M"}
-        client = Client()
-        resp = client.post('/tshirt/', data=post_data)
-        assert len(Tshirt.objects.filter(brand="Volcom", quantity=10)) == 1
+        resp = self.client.post('/tshirt/', data=post_data)
+        assert len(Tshirt.objects.all()) == 1
         assert resp.status_code == 201
 
     def test_get_details_from_a_tshirt(self):
-        Tshirt.objects.create(brand='Volcom', quantity=10, size='L', pk=1)
-        client = Client()
-        resp = client.get('/tshirt/1/')
-        assert resp.json() == {
-            'pk': 1,
-            'brand': 'Volcom',
-            'size': 'L',
-            'quantity': 10
-        }
+        tshirt = Tshirt(brand='Volcom', quantity=10, size='L')
+        tshirt.save()
+        resp = self.client.get(f'/tshirt/{tshirt.pk}/')
+        brand = resp.json()['brand']
+        assert Tshirt.objects.get(brand=brand)
         assert resp.status_code == 200
 
     def test_update_details_from_a_tshirt(self):
-        Tshirt.objects.create(brand='Volcom', quantity=10, size='L', pk=1)
-        put_data = {"brand": "Volcom", "quantity": 5, "size": "XL", "pk": 1}
-        client = Client()
-        resp = client.put(
-            '/tshirt/1/',
+        tshirt = Tshirt(brand='Volcom', quantity=10, size='L')
+        tshirt.save()
+        put_data = {"brand": "Volcom", "quantity": 5, "size": "XL"}
+        resp = self.client.put(
+            f'/tshirt/{tshirt.pk}/',
             data=json.dumps(put_data),
             content_type='application/json')
-        assert resp.json() == {
-            'pk': 1,
-            'brand': 'Volcom',
-            'size': 'XL',
-            'quantity': 5
-        }
+        size = resp.json()['size']
+        assert tshirt == Tshirt.objects.get(size=size)
         assert resp.status_code == 200
 
     def test_delete_tshirt(self):
-        Tshirt.objects.create(brand='Volcom', quantity=10, size='L', pk=1)
-        client = Client()
-        resp = client.delete('/tshirt/1/')
+        tshirt = Tshirt(brand='Volcom', quantity=10, size='L')
+        tshirt.save()
+        resp = self.client.delete(f'/tshirt/{tshirt.pk}/')
         assert resp.status_code == 204
