8ebd02db172f0de0c0043205ef338099ef258f10
Heron Rossi <heron.rossi@hotmail.com>
2018-01-24 12:10:58 -0200

    Refactoring serializers

--- tshirt/serializers.py
+++ tshirt/serializers.py
@@ -1,19 +1,9 @@
 from rest_framework import serializers
+
 from tshirt.models import Tshirt
 
 
-class TshirtSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    brand = serializers.CharField(max_length=150)
-    size = serializers.CharField(max_length=3)
-    quantity = serializers.IntegerField()
-
-    def create(self, validated_data):
-        return Tshirt.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.brand = validated_data.get('brand', instance.brand)
-        instance.size = validated_data.get('size', instance.size)
-        instance.quantity = validated_data.get('quantity', instance.quantity)
-        instance.save()
-        return instance
+class TshirtSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tshirt
+        fields = ('pk', 'brand', 'quantity', 'size', 'date_added')
--- tshirt/tests/test_serializers.py
+++ tshirt/tests/test_serializers.py
@@ -7,19 +7,17 @@ from tshirt.serializers import TshirtSerializer
 @pytest.mark.django_db(transaction=True)
 class TestTshirtSerializer:
     def test_can_serialize_tshirt_model(self):
-        tshirt = Tshirt(brand='Volcom', quantity=10, size='M', pk=1)
+        tshirt = Tshirt(
+            brand='Volcom',
+            quantity=10,
+            size='M',
+        )
         tshirt.save()
         serialized_tshirt = TshirtSerializer(tshirt)
-        assert serialized_tshirt.data == {
-            'pk': 1,
-            'brand': 'Volcom',
-            'size': 'M',
-            'quantity': 10
-        }
+        pk = serialized_tshirt.data['pk']
+        assert tshirt == Tshirt.objects.get(pk=pk)
 
     def test_can_save_serialized_data_to_db(self):
-        tshirt_data = {'pk': 1, 'brand': 'Volcom', 'size': 'M', 'quantity': 10}
+        tshirt_data = {'brand': 'Volcom', 'size': 'M', 'quantity': 10}
         serialized_tshirt = TshirtSerializer(data=tshirt_data)
-        if serialized_tshirt.is_valid():
-            tshirt = serialized_tshirt.save()
-            assert isinstance(tshirt, Tshirt)
+        assert serialized_tshirt.is_valid()
